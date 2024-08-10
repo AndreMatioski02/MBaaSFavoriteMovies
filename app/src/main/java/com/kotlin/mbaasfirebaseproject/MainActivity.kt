7ea8a9b3dec513381dd2b5acb6dd7408cf625aae
AndreMatioski02 <matioskiandre4@gmail.com>
2024-08-10 11:31:33 -0300

    Removing dead code

--- app/src/main/java/com/kotlin/mbaasfirebaseproject/MainActivity.kt
+++ app/src/main/java/com/kotlin/mbaasfirebaseproject/MainActivity.kt
@@ -3,17 +3,14 @@ package com.kotlin.mbaasfirebaseproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.kotlin.mbaasfirebaseproject.navigation.AppNavGraph
-import com.kotlin.mbaasfirebaseproject.ui.screens.login.LoginViewModel
 import com.kotlin.mbaasfirebaseproject.ui.theme.MBaaSFirebaseProjectTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val loginViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         FirebaseApp.initializeApp(this)
         val currentUser = FirebaseAuth.getInstance().currentUser
